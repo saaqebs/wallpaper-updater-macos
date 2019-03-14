761139ae5cb540c0d145aca66e92d893d4268544
saaqebs <34994908+saaqebs@users.noreply.github.com>
2019-03-14 19:26:26 -0400

    main method

---
+++ main.py
@@ -0,0 +1,28 @@
+import wallpaper as wp
+import schedule
+
+'''
+Global variables you may change according to your preferences.
+Information could be found on https://source.unsplash.com/
+'''
+directory = '/Users/home/Documents/Other/wallpaper/wallpaper.jpg'
+collection_url = 'https://source.unsplash.com/collection/3330448/1600x900'
+keywords = ['mountains']
+
+
+def update_wallpaper(is_collection):
+    wp.delete_old_wallpaper(directory)
+    if (is_collection):
+        wp.download_wallpaper_from_collection(collection_url, directory)
+    else:
+        wp.download_wallpaper_from_search(keywords, directory)
+    wp.change_wallpaper(directory)
+
+
+def main():
+    schedule.every().day.at("02:00").do(update_wallpaper)
+    update_wallpaper(True)
+
+
+if __name__ == "__main__":
+    main()
